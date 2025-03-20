16f8753d0b920e211112aca3b13b2e2b19d4b035
Gesley Marinho <gesleymarinho@gmail.com>
2025-03-20 20:28:18 -0300

    criação da class Department entity class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import model.entities.Department;
+
 public class Main {
     public static void main(String[] args) {
+        Department department = new Department(1,"table");
 
-        System.out.println("ahahah ");
-
+        System.out.println(department);
     }
 }
