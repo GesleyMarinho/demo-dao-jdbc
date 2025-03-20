5cd011dc927bed3a044bce4f4efd5b5c74b243bc
Gesley Marinho <gesleymarinho@gmail.com>
2025-03-20 20:46:53 -0300

    criação da class Seller entity class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Department department = new Department(1,"table");
+        Seller seller = new Seller(1,"bob","bob@bob.com",new Date(),4000.0,department);
 
         System.out.println(department);
+
+        System.out.println(seller);
     }
 }
