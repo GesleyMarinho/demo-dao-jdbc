69df05a2b5f028f4793db495f03691c29e97407a
Gesley Marinho <gesleymarinho@gmail.com>
2025-03-20 21:20:28 -0300

    tetse

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 import entities.Department;
 import entities.Seller;
+import modelDao.DaoFactory;
+import modelDao.SellerDao;
 
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Department department = new Department(1,"table");
-        Seller seller = new Seller(1,"bob","bob@bob.com",new Date(),4000.0,department);
+        Department department = new Department(1, "table");
+        Seller seller = new Seller(1, "bob", "bob@bob.com", new Date(), 4000.0, department);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
 
         System.out.println(department);
 
---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,33 @@
+package model.dao.impl;
+
+import entities.Seller;
+import modelDao.SellerDao;
+
+import java.util.List;
+
+public class SellerDaoJDBC implements SellerDao {
+    @Override
+    public void insert(Seller obj) {
+
+    }
+
+    @Override
+    public void update(Seller obj) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public Seller findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return List.of();
+    }
+}
